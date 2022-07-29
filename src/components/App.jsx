22a4c0b8f1ec5ee76ea6da6b808f76f7b250382a
scottsak <48599109+scottsak@users.noreply.github.com>
2022-07-28 23:14:09 -0700

    changed back to server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -96,7 +96,7 @@ function App() {
 
   // gets movie to start the game 
   function getInfoforMovie() {
-    Axios.get("http://localhost:3001/get_movie_info", {
+    Axios.get("https://reeglegame.herokuapp.com/get_movie_info", {
       params: {
         todaysDate: (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear()
       }
