815d28e0e0bf33e60aac26235606ab5d4628aac9
scottsak <48599109+scottsak@users.noreply.github.com>
2022-08-27 17:59:21 -0700

    removed the button press

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -4,7 +4,7 @@ import Axios from 'axios';
 
 const AutoComplete = ({ suggestions, date, setGuesses, guessesAmount, setGuessList, guessList, setBlurImage, blurImage, win, setWin, setGamesPlayed, gamesPlayed}) => {
   const [filteredSuggestions, setFilteredSuggestions] = useState([]);
-  const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(0);
+  // const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(0);
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [input, setInput] = useState("");
 
@@ -86,43 +86,43 @@ const AutoComplete = ({ suggestions, date, setGuesses, guessesAmount, setGuessLi
     // Filter our suggestions that don't contain the user's input
     setInput(e.target.value);
     getSuggestions(e.target.value)
-    setActiveSuggestionIndex(0);
+    // setActiveSuggestionIndex(0);
     setShowSuggestions(true);
   };
 
   const onClick = (e, key) => {
     setFilteredSuggestions([]);
     setInput("");
-    setActiveSuggestionIndex(0);
+    // setActiveSuggestionIndex(0);
     setShowSuggestions(false);
     compareAns(e.target.innerText, key.substring(key.lastIndexOf(',')+1));
   };
 
-  const onKeyDown = (e) => {
-    // User pressed the enter key
-    if (e.keyCode === 13) {
-      setInput("");
-      setActiveSuggestionIndex(0);
-      setShowSuggestions(false);
-      compareAns(filteredSuggestions[activeSuggestionIndex].substring(0, filteredSuggestions[activeSuggestionIndex].lastIndexOf(',')), filteredSuggestions[activeSuggestionIndex].substring(filteredSuggestions[activeSuggestionIndex].lastIndexOf(',')+1));
-    }
-    // User pressed the up arrow
-    else if (e.keyCode === 38) {
-      if (activeSuggestionIndex === 0) {
-        return;
-      }
+  // const onKeyDown = (e) => {
+  //   // User pressed the enter key
+  //   if (e.keyCode === 13) {
+  //     setInput("");
+  //     // setActiveSuggestionIndex(0);
+  //     setShowSuggestions(false);
+  //     compareAns(filteredSuggestions[activeSuggestionIndex].substring(0, filteredSuggestions[activeSuggestionIndex].lastIndexOf(',')), filteredSuggestions[activeSuggestionIndex].substring(filteredSuggestions[activeSuggestionIndex].lastIndexOf(',')+1));
+  //   }
+  //   // User pressed the up arrow
+  //   else if (e.keyCode === 38) {
+  //     if (activeSuggestionIndex === 0) {
+  //       return;
+  //     }
 
-      setActiveSuggestionIndex(activeSuggestionIndex - 1);
-    }
-    // User pressed the down arrow
-    else if (e.keyCode === 40) {
-      if (activeSuggestionIndex - 1 === filteredSuggestions.length) {
-        return;
-      }
+  //     setActiveSuggestionIndex(activeSuggestionIndex - 1);
+  //   }
+  //   // User pressed the down arrow
+  //   else if (e.keyCode === 40) {
+  //     if (activeSuggestionIndex - 1 === filteredSuggestions.length) {
+  //       return;
+  //     }
 
-      setActiveSuggestionIndex(activeSuggestionIndex + 1);
-    }
-  };
+  //     setActiveSuggestionIndex(activeSuggestionIndex + 1);
+  //   }
+  // };
 
   const SuggestionsListComponent = () => {
     return filteredSuggestions.length ? (
@@ -130,10 +130,10 @@ const AutoComplete = ({ suggestions, date, setGuesses, guessesAmount, setGuessLi
         {filteredSuggestions.map((suggestion, index) => {
           let className;
 
-          // Flag the active suggestion with a class
+          {/* // Flag the active suggestion with a class
           if (index === activeSuggestionIndex) {
             className = "suggestion-active";
-          }
+          } */}
 
           return (
             <li className={className} key={suggestion} onClick={event => onClick(event, suggestion)}>
@@ -155,7 +155,7 @@ const AutoComplete = ({ suggestions, date, setGuesses, guessesAmount, setGuessLi
         placeholder={"Guess "+guessesAmount+" of 6"}
         type="text"
         onChange={onChange}
-        onKeyDown={onKeyDown}
+        // onKeyDown={onKeyDown}
         value={input}
       />}
       
